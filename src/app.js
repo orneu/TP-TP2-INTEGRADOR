a8614f37867218c8cc53744c21479b1f0d6d6015
ornellapacino <ornellajazminpacino@gmail.com>
2025-06-17 20:46:56 -0300

    add dependencies

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import express from "express";
+import { config } from "./config/config.js";
+
+const app = express();
---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+const config = {
+  HOST: process.env.HOST ?? "127.0.0.1",
+  PORT: process.env.HOST ?? 3001,
+};
